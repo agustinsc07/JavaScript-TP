560d7b62876136d5b639a2d8a417f2c041a62e3d
agustinsc07 <saezagustin75@gmail.com>
2025-07-23 23:25:42 -0300

    se realizo el quinto ejercicio

---
+++ ejercicio5.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("ingrese el primer numero"))
+let num2 = parseInt(prompt("ingrese el primer numero"))
+document.writeln(
+    "la suma del numero",
+    num1,
+    "con el otro numero",
+    num2,
+)
